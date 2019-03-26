469444135549d7b34a5f2a085562c0eb18b58671
IfMikaelStrid <stridmikael@gmail.com>
2019-03-26 17:05:27 +0100

    CheckPoint

---
+++ ReactCoreHome/ClientApp/src/components/MontyHallContainer.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+import Parallax from 'parallax-js';
+import './src/Content/ContentBlockOne.css';
+export class MontyHallContainer extends Component {
+    displayName = "MontyHall"
+    componentDidMount() {
+        var scene = document.getElementById('MontyScene');
+        var parallaxInstance = new Parallax(scene);
+    }
+    render() {
+        return (
+            <div>MONTY HALL TITLE</div>
+            <div className='blockOneContainer' id='MontyScene'>
+                <div data-depth="0.1">Monty hall text</div>
+                <div className='blockOneRightText'></div>
+            </div>
+        );
+    }
+}
