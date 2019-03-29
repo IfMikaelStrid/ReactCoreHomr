28288c8e18a7d148fa9c3d39ce82256192c394d4
IfMikaelStrid <stridmikael@gmail.com>
2019-03-29 18:11:21 +0100

    Checkpoint 29/3

--- ReactCoreHome/ClientApp/src/components/MontyHallContainer.js
+++ ReactCoreHome/ClientApp/src/components/MontyHallContainer.js
@@ -2,26 +2,30 @@
 import Parallax from 'parallax-js';
 import './src/Content/css/MontyHall.css';
 import fullMonty from './src/Content/Images/Monty.png';
+import door1 from './src/Content/Images/door1.png';
+import door2 from './src/Content/Images/door2.png';
+import door3 from './src/Content/Images/door3.png';
 
 export class MontyHallContainer extends Component {
     displayName = "MontyHall"
     componentDidMount() {
         var scene = document.getElementById('MontyScene');
         var parallaxInstance = new Parallax(scene);
-
     }
+
     render() {
+
         return (
             <div className='MontyContainer' id='MontyScene'>
                 <div className='MontyFlexContainer'>
                     <div className='MontyDoor' id='door1'>
-                        <div className='MontyDoorKnob'></div>
+                        <img src={door1} alt="door1" id='doorimg'/>
                     </div>
                     <div className='MontyDoor' id='door2'>
-                        <div className='MontyDoorKnob'></div>
+                        <img src={door2} alt="door2" id='doorimg'/>
                     </div>
                     <div className='MontyDoor' id='door3'>
-                        <div className='MontyDoorKnob'></div>
+                        <img src={door3} alt="door3" id='doorimg'/>
                     </div>
                     <div className='MontyHimself' >
                         <img src={fullMonty} alt="Monty"/>
