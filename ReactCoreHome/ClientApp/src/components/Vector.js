d72a8cae71196814db59c5643d8337e48cff18f7
IfMikaelStrid <stridmikael@gmail.com>
2019-05-28 13:43:01 +0200

    Vector background added

---
+++ ReactCoreHome/ClientApp/src/components/Vector.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+import './Home.css';
+
+export class Vector extends Component {
+    displayName = Vector.name
+
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.src = './src/script/Particles.js';
+        script.async = true;
+        document.body.appendChild(script);
+    }
+    render() {
+        return (
+            <div className="particle-container">
+                <div id="particles-js"></div>
+                <div className="centered">Hello</div>
+            </div>
+        );
+    }
+}
