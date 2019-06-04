966797ca44626658ade1746c164703f9d59e7df7
IfMikaelStrid <stridmikael@gmail.com>
2019-06-04 08:28:12 +0200

    Prutt

--- ReactCoreHome/ClientApp/src/components/Home.js
+++ ReactCoreHome/ClientApp/src/components/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Parallax from 'parallax-js';
 import { Greeting } from './Greeting.js';
-import { MontyHallContainer } from './MontyHallContainer.js';
-import { ContentBlockOne } from './ContentBlockOne.js';
-import { ContentBlockTwo } from './ContentBlockTwo.js';
+import { AboutSection } from './aboutsection.js';
 import './Home.css';
 
 export class Home extends Component
@@ -19,9 +17,7 @@ export class Home extends Component
                 <div className="flex_background">
                     <Greeting />
                 </div>
-                <MontyHallContainer />
-                <ContentBlockOne />
-                <ContentBlockTwo />
+
             </div>
         );
     }
---
+++ ReactCoreHome/ClientApp/src/components/aboutsection.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+import './src/about.css';
+
+export class AboutSection extends Component {
+    displayName = AboutSection.name
+
+    render() {
+        return (
+            <div className="AboutFlex" >
+                <div className="AboutHeader" >
+                    About me
+                </div>
+                <div className="AboutBody">
+                    Body lorem keblar kublar lirem lorem keblar kublar lorem keblar kublar lirem lorem keblar kubla rlorem keblar kublar lirem lorem keblar kublar lorem keblar kublar lirem lorem keblar kublar
+                </div>
+            </div>
+        );
+    }
+}
